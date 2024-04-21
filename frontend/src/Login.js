b6636a9ca985bfb24f8f78f9bdad1cfcc32094f7
Kaja <pisonova9@uniba.sk>
2024-04-21 12:26:23 +0200

    frontend for login

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,13 +1,30 @@
 
-import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import "./Register.css"
 import {TextField, CardMedia, Button} from '@mui/material';
-import logo from './images/bluelogo.svg'
+import axios from 'axios'
 
 export default function Login () {
+    const [name, setName] = useState('')
+    const [passwd, setPasswd] = useState('')
 
-    return (
+    const handleSubmit = () => {
+        axios.post('/api/login', {
+            username: name,
+            password: passwd
+        }).then(function ({data}) {
+            localStorage.setItem("username", name)
+            localStorage.setItem("token", data.token)
+            window.location.replace(window.location.origin) 
+        }).catch(function (error) {
+            alert(error);
+        });
+    }
+
+    if (localStorage.getItem("token") !== null) {
+        window.location.replace(window.location.origin)            
+    }
+    return (      
         <>
         <div className="container">
             <form>
@@ -16,15 +33,17 @@ export default function Login () {
                 <div className="field">
                     <TextField
                         label='Používateľské meno'
+                        onChange={(e) => setName(e.target.value)}
                     />
                 </div>
                 <div className="field">
                     <TextField
                         label='Heslo'
                         type='password'
+                        onChange={(e) => setPasswd(e.target.value)}
                     />
                 </div>
-                <Button variant="contained">Submit</Button>
+                <Button variant="contained" onClick={handleSubmit}>Submit</Button>
             </form>
         </div>
         </>
