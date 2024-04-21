1600b0938c3d334fb8b811e4651afb609e578d44
Kaja <pisonova9@uniba.sk>
2024-04-21 21:24:58 +0200

    add frontend login logic to home page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -7,6 +7,12 @@ import Button from '@mui/material/Button'
 
 export default function Home () {
     const [events, setEvents] = useState([])
+
+    const HandleLogIn = () =>{
+        if (localStorage.getItem["token"] !== null) {
+            delete localStorage["token"];
+        }
+    }
     
     const getData =async ()=> {
         try {
@@ -29,8 +35,24 @@ export default function Home () {
             <p>Začiatok: {item.start.substring(0,10)} </p>
             <p>Koniec: {item.end.substring(0, 10)}</p>
         </div>)
+    let txt = "Odhlásiť sa";
+    let adr = "/"
+    let welcome = ""
+    if (localStorage["token"] != null) {
+        welcome = "Ste prihlásený ako " + localStorage["username"];
+    }
+    if (localStorage["token"] == null) {
+        txt = "Prihlásiť sa";
+        adr = "/login";
+    }
     return (<>
         <div>
+            <div class="btn">
+                <Button href={adr} onClick={HandleLogIn}>{txt}</Button>
+            </div>
+            <div class='log'>
+                <p>{welcome}</p>
+            </div>
             {myList}
         </div>
     </>)
