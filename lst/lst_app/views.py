e7aa287ae72fc351fef4eb4a57c503a4c0fe2531
Kaja <pisonova9@uniba.sk>
2024-04-21 12:21:45 +0200

    backend for login

--- lst/lst_app/views.py
+++ lst/lst_app/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt 
+import json
+from django.contrib.auth.models import User
+import jwt
+from django.conf import settings
 
-# Create your views here.
+
+@csrf_exempt
+def login(req):
+    body = json.loads(req.body)
+    name, password = body["username"], body["password"]
+    print(name, password)
+    users = User.objects.filter(username=name)
+
+    if users.count() == 0:
+        return JsonResponse({"message": "No user found"}, status=401)
+
+    user = users.first()
+
+    if not user.check_password(raw_password=password):
+        return JsonResponse({"message": "Wrong password"}, status=401)
+
+    token = jwt.encode({"name": name}, key=settings.SECRET_KEY, algorithm="HS256")
+    return JsonResponse({"token": token})
+    
