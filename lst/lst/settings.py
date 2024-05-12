3d1059b25985e7686b292db99dbcabb9e8587eef
Kaja <pisonova9@uniba.sk>
2024-05-12 20:16:57 +0200

    change in settings

--- lst/lst/settings.py
+++ lst/lst/settings.py
@@ -37,7 +37,7 @@ ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = 'static-files/'
 
 # Application definition
 
